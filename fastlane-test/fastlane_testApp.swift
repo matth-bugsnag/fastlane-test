943b53ab9465820fe79abc4824a76342944c5734
matth-bugsnag <113024072+matth-bugsnag@users.noreply.github.com>
2023-01-26 14:23:31 +0000

    Initial Commit

---
+++ fastlane-test/fastlane_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastlane_testApp.swift
+//  fastlane-test
+//
+//  Created by Matthew Howells on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct fastlane_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
